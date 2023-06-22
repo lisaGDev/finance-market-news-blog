c7fa4b478f74ed36afed53ac906acfd81a0e9d86
Lisa Galva <lisagalva@Lisas-Air.fios-router.home>
2023-06-22 00:17:55 -0400

    add json mock data

--- __main__.py
+++ __main__.py
@@ -1,28 +1,41 @@
 from flask import Flask, render_template, url_for, redirect
+from firebase_data.data import get_data
 app = Flask(__name__)
 
-news_articles = [{ 
-    "name": "My Dog Missin'",
-    "url": "/my_dog_missin"
-}]
+news_articles = get_data('/Users/lisagalva/Desktop/Python_Projects/finance-market-news-blog/firebase_data/articles.json')
 
 @app.route('/default')
 def index():
     return render_template("layout.html")
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def detail():
     return render_template("posts.html", news_articles=news_articles)
 
 @app.route('/my_dog_missin')
 def my_dog_missin():
-    return render_template("single.html")
+    return render_template("dog.html")
+
+@app.route('/food_crisis')
+def food_crisis():
+    return render_template("crisis.html")
+
+@app.route('/climate_change')
+def climate_change():
+    return render_template("climate.html")
 
 @app.route("/<article>")
 def articles(article):
-    if article == 'single':
+    if article == 'climate':
         return redirect(url_for('my_dog_missin'))
-    return "404 Article Not Found"
+    if article == 'crisis':
+        return redirect(url_for('food_crisis'))
+    if article == 'dog':
+        return redirect(url_for('climate_change'))
+    # return "404 Article Not Found"
 
 if __name__ == '__main__':
+    # print(get_data())
+    # for news_article in get_data():
+    # print(news_articles)
     app.run()
